478fa0e5a6d15ad618bf49451398d1561128485a
Clayton Collins <claytonccollins@gmail.com>
2017-02-10 14:13:00 -0800

    move edit box to replace clicked contact

--- web/scripts.js
+++ web/scripts.js
@@ -2,7 +2,7 @@ $(function() {
   $('.contact').click(function(e) {
     var clicked = $(this).attr('id');
     $.post('/select_contact', {clicked: clicked}, function(response){
-      $('.edit_contact').html(response);
+      $('.edit_contact'+clicked).html(response);
     });
-  })
+  });
 });
